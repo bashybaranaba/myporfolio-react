effd7eecdf9c90538b2c48f382b85347fb1e51e4
bashybaranaba <bashybaranaba@gmail.com>
2024-01-28 20:19:53 +0300

    feat: make about section responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,21 +15,22 @@ export default function About() {
   return (
     <Container component="main" maxWidth="md">
       <Grid container spacing={1}>
-        <Grid item xs={3} md={3}>
+        <Grid item xs={12} md={3}>
           <Avatar
             alt="Baranaba Mugabane"
             src="assets/images/me.jpeg"
             sx={{ width: 160, height: 160 }}
           />
         </Grid>
-        <Grid item xs={9} md={9}>
+        <Grid item xs={12} md={9}>
           <Typography
             variant="h2"
             component="div"
             sx={{
               fontWeight: 700,
               mb: 2,
-              fontSize: { xs: "2rem", md: "4rem" },
+              fontSize: { xs: "2.6rem", md: "4rem" },
+              ml: { xs: 2, md: 0 },
             }}
           >
             Hi, I'm Baranaba 👋
@@ -38,28 +39,44 @@ export default function About() {
             variant="h4"
             component="div"
             sx={{
-              mb: 2,
+              mb: { xs: 0, md: 2 },
               display: "flex",
-              fontSize: { xs: "2rem", md: "2.3rem" },
+              fontSize: { xs: "1.5rem", md: "2.3rem" },
+              ml: { xs: 2, md: 0 },
             }}
           >
             {"👨🏾‍💻 I am a passionate" + " "}
             <span style={{ color: "#333", margin: 2 }} />
-            <TypingAnimator
-              textArray={[" Developer", " Artist", " Designer"]}
-              cursorColor="#333"
-              textColor="black"
-              fontSize="38px"
-              loop
-              typingSpeed={60}
-              delaySpeed={1000}
-              backspace
-              height="60px"
-            />{" "}
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <TypingAnimator
+                textArray={[" Developer", " Artist", " Designer"]}
+                cursorColor="#333"
+                textColor="black"
+                fontSize={"38px"}
+                loop
+                typingSpeed={60}
+                delaySpeed={1000}
+                backspace
+                height="60px"
+              />
+            </Box>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
+              <TypingAnimator
+                textArray={[" Developer", " Artist", " Designer"]}
+                cursorColor="#333"
+                textColor="black"
+                fontSize={"24px"}
+                loop
+                typingSpeed={60}
+                delaySpeed={1000}
+                backspace
+                height="60px"
+              />
+            </Box>{" "}
           </Typography>
         </Grid>
 
-        <Grid item xs={6} md={6}>
+        <Grid item xs={12} md={6}>
           <Box
             sx={{
               p: 3,
@@ -67,6 +84,7 @@ export default function About() {
               borderRadius: 2,
               border: 1,
               borderColor: "#0097a7",
+              display: { xs: "flex", md: "block" },
             }}
           >
             <SchoolOutlinedIcon color="primary" sx={{ fontSize: 40 }} />
@@ -75,6 +93,7 @@ export default function About() {
               component="div"
               sx={{
                 mb: 2,
+                ml: { xs: 2, md: 0 },
                 fontSize: { xs: "0.8rem", md: "1rem" },
                 fontWeight: 500,
               }}
@@ -84,7 +103,7 @@ export default function About() {
             </Typography>
           </Box>
         </Grid>
-        <Grid item xs={6} md={6}>
+        <Grid item xs={12} md={6}>
           <Box
             sx={{
               p: 3,
@@ -92,6 +111,7 @@ export default function About() {
               borderRadius: 2,
               border: 1,
               borderColor: "#0097a7",
+              display: { xs: "flex", md: "block" },
             }}
           >
             <AccountBalanceOutlinedIcon color="primary" sx={{ fontSize: 40 }} />
@@ -100,6 +120,7 @@ export default function About() {
               component="div"
               sx={{
                 mb: 2,
+                ml: { xs: 2, md: 0 },
                 fontSize: { xs: "0.8rem", md: "1rem" },
                 fontWeight: 500,
               }}
